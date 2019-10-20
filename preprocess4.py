fa5516c380fe72187cd21c9da3f8912c35ca6c88
Tanmay Baweja <31308303+Tanmay98@users.noreply.github.com>
2019-10-20 14:20:30 +0530

    Update preprocess4.py

--- preprocess4.py
+++ preprocess4.py
@@ -1,3 +1,4 @@
+#0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -12,78 +13,106 @@ from random import shuffle
 from mtcnn.mtcnn import MTCNN 
 
 image_size = 150
-data_dir = "E:/DL/facial-expression/augmented-images"
-# df = pd.read_csv('fer2013/fer2013.csv')
+data_dir = "E:/DL/facial-expression/generated-images"
 
-#0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
+X = np.zeros(829707440).reshape(179435,4624)
 
-X = np.zeros(197296).reshape(197296,1)
-y = pickle.load(open("balanced-labels", "rb"))
-y = np.array(y)
+name = 0
+temp = 0 
+b = 0
 
+#this is to identify some of the missing generated images in our generated images folder
+list_img = []
+for images in os.listdir(data_dir):
+	image = os.path.join(data_dir, images)
+	r = image[41:]
+	meh = r.split('_')
+	name = int(meh[0])
+	list_img.append(name)
+	
+listy = [i+1 for i in range(35887)]
+missing = []
+for i in listy:
+	if list_img.count(i) < 6:
+		missing.append(i)
+
+#balancing the generated images dataset
+for images in tqdm(os.listdir(data_dir)):
+	image = os.path.join(data_dir, images)
+	r = image[41:]
+	meh = r.split('_')
+	name = int(meh[0])
+	name2 = int(meh[1][:-4])
+
+	flag = 0
+	
+	for i in missing:
+		if i == name:
+			flag = 1
+
+	if flag == 0:
+		if name2 == 1:
+			os.remove(image)
+
+#preparing our X 
 detector = MTCNN()
-
-for img in os.listdir(data_dir):
+tempo = 1
+for img in tqdm(os.listdir(data_dir)):
 	image = os.path.join(data_dir, img)
 	img_array = cv2.imread(image)
 	new_array = cv2.resize(img_array, (image_size,image_size), interpolation=cv2.INTER_CUBIC)
 	
+	rects_list = []
+
 	p = "shape_predictor_68_face_landmarks.dat"
 	# detector = dlib.get_frontal_face_detector()
 	predictor = dlib.shape_predictor(p)
 	result = detector.detect_faces(new_array)
-	# rect = result[0]('box')
-	rects = result[0]['box']
-	
-	# print(bounding_box)
-	# cv2.rectangle(new_array,
- #              (bounding_box[0], bounding_box[1]),
- #              (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
- #              (0,155,255),
- #              2)
-	# plt.imshow(new_array, cmap='gray')
-	# plt.show()	
 
-	# Get faces into webcam's image
-	# rects = detector(new_array, 1)
-	# print("Number of faces detected: {}".format(len(rects)))
+	if result:
+		rects = result[0]['box']
+		face_location = dlib.rectangle(rects[0], rects[1], rects[2], rects[3])
+		rects_list.append(face_location)
+		
+		# plotting the image with its face detected
+		cv2.rectangle(new_array,
+	              (rects[0], rects[1]),
+	              (rects[0]+rects[2], rects[1] + rects[3]),
+	               (0,155,255),
+	              2)
+		plt.imshow(new_array, cmap='gray')
+		plt.show()	
 
-	# For each detected face, find the landmark.
-	for (i, rect) in enumerate(rects):
-	    # Make the prediction and transfom it to numpy array
-	    shape = predictor(new_array, rect)
-	    shape = face_utils.shape_to_np(shape)
-	    print(shape)
+		# For each detected face, find the landmark.
+		for (i, rect) in enumerate(rects_list):
+		    # Make the prediction and transfom it to numpy array
+		    shape = predictor(new_array, rect)
+		    shape = face_utils.shape_to_np(shape)
 
-	break
+		#for calculating euclidian distances between 68 facial points
+		distances = []
+		for i in shape:
+			for j in range(len(shape)):
+				dist = math.sqrt((shape[j][0]-i[0])**2 + (shape[j][1]-i[1])**2)
+				distances.append(dist)
+	else:
+		distances = []
+		distances.append('NaN')
 
-	
-# 	#for calculating euclidian distances between 68 facial points
-# 	distances = []
-# 	for i in shape:
-# 		for j in range(len(shape)):
-# 			dist = math.sqrt((shape[j][0]-i[0])**2 + (shape[j][1]-i[1])**2)
-# 			distances.append(dist)
-	
-##	X.append(distances)    
-# 	X[int(name) - 1] = distances
+	# assigning distance value to the proper index in X numpy array
+	r = image[41:]
+	meh = r.split('_')
+	name = int(meh[0])
+
+	if temp != name:
+		b = 0
+
+	temp = name
+	X[(name-1)*5 + b] = distances # a +(n-1)d can be thought as of ap series which gives the general term and we can fetch easily each random term
+	b+=1
+
+pickle_out = open("X_pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
 
 
-# pickle_out = open("X_pickle", "wb")
-# pickle.dump(X, pickle_out)
-# pickle_out.close()
-
-# pickle_out = open("y_pickle", "wb")
-# pickle.dump(y, pickle_out)
-# pickle_out.close()
-
-
-# Thanks guys, I figured it out, I was supposed to add a try/exception so my code could bypass "ugly" images:
-
-#     try:
-#     path=os.path.join(mypath,n)
-#         img=cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-#         img=cv2.resize(img, (img_rows,img_cols))
-
-#     except Exception as e:
-#         print(str(e))
